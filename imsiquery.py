659e9b846207b4e65c12e025ddc90de984cf8cde
faco <f4co@yahoo.com>
2017-12-14 02:11:26 +0700

    test dulu aja lah..

---
+++ imsiquery.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import sys
+import sqlite3
+
+#./OpenBTSCLI sendsms nomorimsi nomorpengirim isisms
+
+if len(sys.argv) < 4:
+	print "Usage : ./imsiquery.py database nomorpengirim isisms"
+	print 'Contoh: ./imsiquery.py scanimsi_513.db 123456 "ini test sms"'
+	sys.exit()
+
+db = sqlite3.connect(sys.argv[1])
+file = "%s.sh" % sys.argv[2]
+f = open(file,'w')
+
+cursor = db.cursor()
+for row in db.execute('SELECT imsi FROM observations'):
+	s = './OpenBTSCLI sendsms %s %s "%s"\n' % (row[0].replace(" ",""), sys.argv[2], sys.argv[3])
+	f.write(s)
+f.close()	
+
