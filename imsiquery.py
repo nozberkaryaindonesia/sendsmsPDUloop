38d2143bb5f529866cc191a60686b403cd91467c
faco <f4co@yahoo.com>
2017-12-14 02:25:29 +0700

    bikin result file jadi 755

--- imsiquery.py
+++ imsiquery.py
@@ -2,6 +2,7 @@
 
 import sys
 import sqlite3
+import os
 
 #./OpenBTSCLI sendsms nomorimsi nomorpengirim isisms
 
@@ -13,10 +14,10 @@ if len(sys.argv) < 4:
 db = sqlite3.connect(sys.argv[1])
 file = "%s.sh" % sys.argv[2]
 f = open(file,'w')
-
 cursor = db.cursor()
 for row in db.execute('SELECT imsi FROM observations'):
 	s = './OpenBTSCLI sendsms %s %s "%s"\n' % (row[0].replace(" ",""), sys.argv[2], sys.argv[3])
 	f.write(s)
-f.close()	
+f.close()
+os.chmod(file, 493)	
 
